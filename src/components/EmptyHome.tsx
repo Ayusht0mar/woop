ce629171a80005214efcf9d0cd06795a8e12b9da
Ayush tomar <ayushtomar388@gmail.com>
2025-01-09 23:45:29 +0530

    EmptyHomepage component created

---
+++ src/components/EmptyHome.tsx
@@ -0,0 +1,9 @@
+const EmptyHome = () => {
+    return ( 
+        <div>
+            <h2 className="text-center mt-[5%] text-6xl leading-normal uppercase font-bold text-balance text-zinc-700 max-w-6xl mx-auto">You can't do everthing, But you can do anything. <br /> So plan what you want to do.</h2>
+        </div>
+     );
+}
+ 
+export default EmptyHome;
